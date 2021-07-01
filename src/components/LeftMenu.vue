62300662cbe57149e61ec801f02a01ab41dcab6a
sjahn <asj214@naver.com>
2021-07-01 19:06:32 +0900

    update

--- src/components/LeftMenu.vue
+++ src/components/LeftMenu.vue
@@ -3,15 +3,6 @@
     <template v-for="(menu, key) in menus">
       <Tree :node="menu" :key="key"></Tree>
     </template>
-    <!-- <template v-for="(menu, key) in menus">
-      <el-menu-item :key="key" :index="menu.page.path" v-if="!menu.children">{{ menu.name }}</el-menu-item>
-      <el-submenu :key="key" v-if="menu.children" :index="menu.page.path">
-        <template slot="title">{{ menu.name }}</template>
-        <template v-for="child in menu.children">
-          <el-menu-item :index="child.page.path" :key="child.id">{{ child.name }}</el-menu-item>
-        </template>
-      </el-submenu>
-    </template> -->
   </el-menu>
 </template>
 <script>
@@ -46,7 +37,6 @@ export default {
       const { status, data } = await this.axios.get('menus')
       if (status === 200) {
         this.menus = generateNested(data)
-        console.log(this.menus)
         this.visible = true
       }
     }
